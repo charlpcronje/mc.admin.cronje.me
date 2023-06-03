98ef44d68055d6e8caf2e9e0f4c474f9597f5b9c
CHarl Cronje <charlcp@gmail.com>
2023-06-03 18:54:34 +0200

    Added malls route and an /pages/malls/index.vue

---
+++ pages/malls/index.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'dashboard',
+})
+</script>
+
+<template>
+  <PageWrapper>
+    <PageHeader>
+      <PageTitle :text="$t('pages.malls.title')" class="capitalize" />
+    </PageHeader>
+    <PageBody>
+      <PageSection>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Mall Name</th>
+              <th>URL</th>
+              <th>Poster</th>
+              <th>User Icon</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="img"></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </PageSection>
+      <PageSection> </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
