b386bce65fb6b8472f0c4fe984eed9919886b7bf
CHarl Cronje <charlcp@gmail.com>
2023-06-08 08:14:40 +0200

    Add nuxt-auth to header buttons

--- components/Page/Navbar.vue
+++ components/Page/Navbar.vue
@@ -1,6 +1,18 @@
 <script lang="ts" setup>
 import { AppConfigInput } from '@nuxt/schema'
 
+const { status, signIn, signOut } = useAuth();
+const loggedIn = computed(() => status.value === 'authenticated');
+  
+
+async function handleSignIn() {
+  //await signIn();
+}
+async function handleSignOut() {
+  //await signOut();
+}
+
+
 export interface IMenuItem {
   type: 'link' | 'button'
   text: string
@@ -13,17 +25,17 @@ const app = useAppConfig() as AppConfigInput
 const menus = computed((): IMenuItem[] => [
   {
     type: 'link',
-    text: t('pages.getting-started.nav'),
-    route: { name: 'getting-started' },
+    text: t('pages.malls.nav'),
+    route: { name: 'malls' },
   },
-  { type: 'link', text: t('pages.blank.nav'), route: { name: 'blank' } },
-  { type: 'link', text: t('pages.test.nav'), route: { name: 'test' } },
-  { type: 'link', text: t('pages.post.nav'), route: { name: 'post' } },
-  { type: 'link', text: t('pages.setting.nav'), route: { name: 'setting' } },
+  { type: 'link', text: t('pages.notes.nav'), route: { name: 'notes' }},
+  /*{ type: 'link', text: t('pages.test.nav'), route: { name: 'test' }},*/
+    { type: 'link', text: t('pages.post.nav'), route: { name: 'post' }},
+    { type: 'link', text: t('pages.setting.nav'), route: { name: 'setting'}},
   {
     type: 'button',
-    text: t('pages.dashboard.nav'),
-    route: { name: 'dashboard' },
+    text: (loggedIn) ? t('auth.signOut') :  t('auth.signIn'),
+    route: { name: 'login' },
   },
 ])
 </script>
@@ -46,40 +58,34 @@ const menus = computed((): IMenuItem[] => [
     </template>
     <template #menu>
       <div class="relative hidden lg:flex items-center ml-auto">
-        <nav
-          class="text-sm leading-6 font-semibold text-gray-600 dark:text-gray-300"
-          role="navigation"
-        >
+        <nav class="text-sm leading-6 font-semibold text-gray-600 dark:text-gray-300" role="navigation">
           <ul class="flex items-center space-x-8">
             <li v-for="(item, i) in menus" :key="i">
               <Anchor
                 v-if="item.type === 'link'"
                 :to="item.route ? item.route : undefined"
                 :href="item.href ? item.href : undefined"
-                class="hover:no-underline hover:text-slate-900 hover:dark:text-white capitalize"
-                >{{ item.text }}</Anchor
-              >
+                class="hover:no-underline hover:text-slate-900 hover:dark:text-white capitalize">
+                {{ item.text }}
+              </Anchor>
               <Button
                 v-else-if="item.type === 'button'"
                 :text="item.text"
                 size="xs"
                 class="font-extrabold capitalize"
                 :to="item.route ? item.route : undefined"
-                :href="item.href ? item.href : undefined"
-              />
+                :href="item.href ? item.href : undefined"/>
             </li>
           </ul>
         </nav>
         <div
-          class="flex space-x-4 border-l ml-6 pl-6 border-gray-900/10 dark:border-gray-50/[0.2]"
-        >
+          class="flex space-x-4 border-l ml-6 pl-6 border-gray-900/10 dark:border-gray-50/[0.2]">
           <LanguageSwitcher />
           <ThemeSwitcher />
           <Anchor
             class="hover:no-underline hover:text-slate-900 hover:dark:text-white text-lg flex self-center items-center"
             href="https://github.com/charlpcronje/mc.admin.cronje.me"
-            title="Github"
-          >
+            title="Github">
             <IconMdi:github-face />
           </Anchor>
         </div>
@@ -98,29 +104,27 @@ const menus = computed((): IMenuItem[] => [
                 :class="{
                   'pb-2 mb-2 border-b border-gray-900/10 dark:border-gray-50/[0.2]':
                     item.type === 'link',
-                }"
-              >
+                }">
                 <Anchor
                   v-if="item.type === 'link'"
                   :to="item.route ? item.route : undefined"
                   :href="item.href ? item.href : undefined"
                   class="flex-1 hover:no-underline capitalize"
-                  >{{ item.text }}</Anchor
-                >
+                  >{{ item.text }}</Anchor>
                 <Button
                   v-else-if="item.type === 'button'"
                   :text="item.text"
                   size="xs"
                   class="flex-1 font-extrabold capitalize"
                   :to="item.route ? item.route : undefined"
-                  :href="item.href ? item.href : undefined"
-                />
+                  :href="item.href ? item.href : undefined"/>
               </li>
             </ul>
           </nav>
           <div class="mt-6 text-sm font-bold capitalize">
             {{ $t('components.theme_switcher.change_theme') }}
           </div>
+          <!--
           <div class="mt-2">
             <ThemeSwitcher type="select-box" />
           </div>
@@ -130,20 +134,22 @@ const menus = computed((): IMenuItem[] => [
           <div class="mt-2">
             <LanguageSwitcher type="select-box" />
           </div>
+        -->
         </ActionSheetBody>
+        <!--
         <Button
           type="secondary"
           title="Github"
-          href="https://github.com/charlpcronje/mc.admin.cronje.me"
-        >
+          href="https://github.com/charlpcronje/mc.admin.cronje.me">
           <IconMdi:github-face />
           <span class="ml-1">Github</span>
         </Button>
+        
         <Button
           text="Close"
           type="secondary"
-          @click.prevent="toggleOptions(false)"
-        />
+          @click.prevent="toggleOptions(false)"/>
+        -->
       </ActionSheet>
     </template>
   </BuilderNavbar>
