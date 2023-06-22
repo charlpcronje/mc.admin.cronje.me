d0fd4cc5e13da4b6900fe08a32afa598520cd029
CHarl Cronje <charlcp@gmail.com>
2023-06-22 18:54:49 +0200

    Updated the global helpers to rather all be in one class

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,16 @@
 
 (function(global) {
-    global.isDev = ()=> {
-        return process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev';
-    }
-    
-    global.isProd = ()=> {
-        return process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod';
-    }
-    
-    global.isStage = ()=> {
-        return process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'stage';
+    global.ENV = class {
+        // process.env.NODE_ENV is set in .env file
+        static get isDev() {
+            return process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev';
+        }
+        static get isProd() {
+            return process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod';
+        }
+        static get isStage() {
+            return process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'stage';
+        }
     }
 })(globalThis)
 
